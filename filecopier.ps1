8a436de4d8e40ba051ea66337a42cdfc8e8e155d
vivpandey13 <44554220+vivpandey13@users.noreply.github.com>
2019-02-13 23:37:04 -0600

    Delete filecopier.ps1

--- filecopier.ps1
+++
@@ -1,3 +0,0 @@
-move C:\Webcetera\Source\EzLynx\Windows\WindowsServices\EzLynxDownloaderService\test\DownloaderController.NUnitTests\UploadedFiles\*.al3 \\cimtest.webcetera.com\Downloads
-move C:\Webcetera\Source\EzLynx\Windows\WindowsServices\EzLynxDownloaderService\test\DownloaderController.NUnitTests\TempUploadedFiles\*.al3 \\cimtest.webcetera.com\Downloads
-Remove-Item c:\webcetera\al3\*al3 -Force -Recurse
