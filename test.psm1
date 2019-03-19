9558d1eaf313ac9af02fcb2ec18166c6abd1b60b
vivpandey13 <44554220+vivpandey13@users.noreply.github.com>
2019-03-19 04:55:51 -0500

    Delete test.psm1

--- test.psm1
+++
@@ -1,7 +0,0 @@
-function Invoke-Application {
-    param( [switch] $ls = $false,  [switch] $add = $false)
-$url = Get-Application -ls $ls -add $add $args[0] $args[1]
-if($null -ne $url){
-    Invoke-Item $url
-}
-}
