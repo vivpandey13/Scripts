6a763b849bc6c2e136cdf640a28b6da1a773dd5d
vivpandey13 <44554220+vivpandey13@users.noreply.github.com>
2019-03-19 05:12:37 -0500

    Create InvokeApplication.psm1

---
+++ InvokeApplication.psm1
@@ -0,0 +1,36 @@
+Import-Module .\GetApplication.psm1    
+
+function Invoke-Application {    
+    [Alias("launch")]
+    param( 
+        [switch] $ls = $false, 
+        [switch] $add = $false,
+        [switch] $like = $false
+    )
+
+    if ($ls) {
+        Get-Application -ls
+    }
+    elseif ($add) {
+        Get-Application -add $args[0] $args[1]
+    }
+    elseif ($like) {    
+        
+        Get-Application -like $args[0]
+    }
+    elseif ($null -ne $args[0]) { 
+        $lst = $args[0].split(',')
+        foreach ($app in $lst) {
+            $element = Get-Application $app
+            if ($null -ne $element -and $null -ne $element.url) {
+                Invoke-Item $element.url
+            }
+        }
+    }
+}
+
+# Remove-Module InvokeApplication
+# Import-Module .\InvokeApplication.psm1
+# Invoke-Application vs, api
+# launch  vs
+# launch -like policy
